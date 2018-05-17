61f5fba282a204d8669b2f3beda0f12c4ce0624a
Iaroslav Nikishyn <iaroslav.nikishyn@7digital.com>
2018-05-17 19:48:48 +0300

    working with BFS

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,47 +6,70 @@ namespace Test
 {
     class Program
     {
-        private static readonly Dictionary<int, int[]> graph = new Dictionary<int, int[]>();
+        private static readonly Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
 
         static void Main(string[] args)
         {
-            var allNodes = new List<int>();
-            foreach (var y in Enumerable.Range(1, 2))
+            int id = 0;
+            int[][] allNodes = new int[3][];
+            for (int i = 0; i < 3; i++)
             {
-                allNodes.Add(y);
+                allNodes[i] = new int[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    allNodes[i][j] = ++id;
+                }
             }
 
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var currId = allNodes[i][j];
+                    graph[currId] = Neighbors(allNodes, i, j);
+                }
+            }
 
+            var result = Search(3);
         }
 
         static int Search(int id)
         {
-            var queue = new Queue<int>(graph[id]);
+            var queue = new Queue<int>(graph[1]);
             while (queue.Any())
             {
                 int node = queue.Dequeue();
                 if (node == id) return node;
 
-                queue = new Queue<int>(queue.ToArray().Concat(graph[id]));
+                queue = new Queue<int>(queue.ToArray().Concat(graph[node]));
             }
 
             return -1;
         }
 
-        static void Neighbors(int node)
+        static List<int> Neighbors(int[][] allNodes, int x, int y)
         {
-            var dirs = new List<int>()
-            {
-                1,0 } },
-                new [,] { { 1,0 } },
-                new [,] { { 1,0 } },
-                new [,] { { 1, } }
-            };
-            var result = new List<int[,]>();
+            int[][] dirs = new[] {new[] {1, 0}, new[] {0, 1}, new[] {-1, 0}, new[] {0, -1}};
+            var result = new List<int>();
+
             foreach (var dir in dirs)
             {
-                result.Add(new[,] { { node[0] + dir[0] } });
+                var neighborX = x + dir[0];
+                var neighborY = y + dir[1];
+
+                neighborX = neighborX < 0 ? 0 : neighborX;
+                neighborY = neighborY < 0 ? 0 : neighborY;
+
+                if (neighborX >= allNodes.Length || neighborY >= allNodes[y].Length)
+                    continue;
+
+                if(neighborX == x && neighborY == y)
+                    continue;
+
+                result.Add(allNodes[neighborX][neighborY]);
             }
+
+            return result;
         }
     }
 }
